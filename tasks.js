a02c4306dd9b6608fd702a263af07e683f4ff0bf
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-28 17:09:38 -0500

    step 6

--- tasks.js
+++ tasks.js
@@ -1,5 +1,6 @@
 $(document).on('ready', function(){
 
+	//event listeners
 	$('#add-button').on('click', function(event){
 		var content = $('#to-add').val();
 		$('ol').append('<li>' + content + '</li>');
@@ -18,4 +19,13 @@ $(document).on('ready', function(){
 		$(this).remove();
 		$('#flash').animate({'opacity': 0});
 	});
-});
+
+	$('#save-button').on('click', function(event){
+		var list = $('ol').html();
+		localStorage.setItem('Ignacio', list);
+	});
+
+	//run on page load
+	if(localStorage.getItem('Ignacio')){
+		$('ol').append(localStorage.getItem('Ignacio'));
+	};});
