6516f6df73500dccd4dbbc949b400ea236991b8f
Ignacio Prado <ignacio@gatogordoweb.com>
2015-10-28 16:12:17 -0500

    step 5

--- tasks.js
+++ tasks.js
@@ -1,7 +1,21 @@
 $(document).on('ready', function(){
+
 	$('#add-button').on('click', function(event){
 		var content = $('#to-add').val();
 		$('ol').append('<li>' + content + '</li>');
 		$('#to-add').val('');
+	});
+
+	$('body').on('mouseover', 'li', function(){
+		$('#flash').animate({'opacity': 1});
+	});
+
+	$('body').on('mouseleave', 'li', function(){
+		$('#flash').animate({'opacity': 0});
+	});
+
+	$('body').on('click', 'li', function(event){
+		$(this).remove();
+		$('#flash').animate({'opacity': 0});
 	});
 });
